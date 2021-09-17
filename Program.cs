5a26a2a965b3259ae812bd2f4a2b1deaa2f95aba
Felipe Francisco <fefarias97@gmail.com>
2021-09-17 20:45:35 -0300

    Resolução exercício AreaTrianguloRet

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace areatrianguloret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double medidaBase, medidaAltura, medidaArea;
+            Console.Write("Qual é a base..: ");
+            medidaBase = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Qual é a altura: ");
+            medidaAltura = Convert.ToDouble(Console.ReadLine());
+
+            medidaArea = (medidaBase * medidaAltura) /2;
+
+            Console.WriteLine($"Área do Triangulo: {medidaArea}");
+
+        }
+    }
+}
